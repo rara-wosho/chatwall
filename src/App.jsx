82f7d638ecc7e09d07c39b79ebe53f70b965eb0e
Rara <deverinth88@gmail.com>
2025-09-05 21:58:49 +0800

    lazy push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Allroutes from "./routes/Allroutes";
 
 function App() {
-  return (
-    <>
-      <Allroutes />
-    </>
-  );
+    // lazy push sept 5 2025
+    return (
+        <>
+            <Allroutes />
+        </>
+    );
 }
 
 export default App;
